68bf5ed966209befd3c16522e2f1757ec3e2cc7f
lekhalinh <lekhalinh.oppo@gmail.com>
2023-10-21 14:36:55 +0700

    create source

---
+++ app/@function/wsCode.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.dongdoi.com";
+export const BUNNY_URL = "http://dongdoi.b-cdn.net";
+export const API_SPORT = "https://isports.thethao789.com";
+export const API_SABA = "https://www.saba.vn";
+export const SITE_URL = "https://dongdoi.com";
+export const FRAME_ISPORT = "https://stream.raysports.live/vi/football?token=72qdeh1l6hgu5rl"
